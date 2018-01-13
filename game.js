1a2653eeb71f770a06bbcc48eb0b6aff93295fd4
Kuznetsov Alexei <akuznetsov@elewise.com>
2018-01-13 12:53:29 +0400

    * added player control from keyboard

--- game.js
+++ game.js
@@ -6,23 +6,25 @@ window.onload = function() {
         update: update
     });
 
-    //var logo;
-
     function preload() {
-        //game.load.image('logo', 'phaser.png');
         game.load.image('player', 'assets/player.png');
         game.load.image('wall', 'assets/wall.png');
     }
 
     function create() {
-        //logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-        //logo.anchor.setTo(0.5, 0.5);
         game.physics.startSystem(Phaser.Physics.ARCADE);
-        game.world.enableBody = true;
+        game.time.desiredFps = 30;
+        game.physics.arcade.gravity.y = 250;
+
+        this.player = game.add.sprite(70, 100, 'player');
+        game.physics.enable(this.player, Phaser.Physics.ARCADE);
+        this.player.body.bounce.y = 0.2;
+        this.player.body.collideWorldBounds = true;
+        this.player.body.setSize(20, 20);
 
         this.cursor = game.input.keyboard.createCursorKeys();
-        this.player = game.add.sprite(70, 100, 'player');
-        this.player.body.gravity.y = 800;
+        this.jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+        this.jumpTimer = 0;
 
         createLevel();
     }
@@ -45,15 +47,27 @@ window.onload = function() {
             for (var j = 0; j < level[i].length; j++) {
                 if (level[i][j] == 'x') {
                     var wall = game.add.sprite(30 + 20 * j, 30 + 20 * i, 'wall');
+                    //wall.body.immovable = true;
                     this.walls.add(wall);
-                    wall.body.immovable = true;
                 }
             }
         }
     }
 
     function update() {
-        //logo.angle += 1;
         game.physics.arcade.collide(this.player, this.walls);
+
+        if (this.cursor.left.isDown) {
+            this.player.body.velocity.x = -200;
+        } else if (this.cursor.right.isDown) {
+            this.player.body.velocity.x = 200;
+        } else {
+            this.player.body.velocity.x = 0;
+        }
+
+        if (this.jumpButton.isDown && this.player.body.onFloor() && game.time.now > this.jumpTimer) {
+            this.player.body.velocity.y = -250;
+            this.jumpTimer = game.time.now + 750;
+        }
     }
 };
